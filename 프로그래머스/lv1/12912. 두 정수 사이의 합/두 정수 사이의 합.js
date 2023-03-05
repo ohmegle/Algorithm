3650d93a6caaa875c876447ed9a295e502407a43
Jang Eun-Ji <110921798+ohmegle@users.noreply.github.com>
2023-03-05 22:42:07 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -0,0 +1,10 @@
+function solution(a, b) {
+    let answer = 0;
+    let min = Math.min(a,b);
+    let max = Math.max(a,b);
+    
+    for(let i = min; i <= max; i++) {
+        answer += i;
+    }
+    return answer;
+}
