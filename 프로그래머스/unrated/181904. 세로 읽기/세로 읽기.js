42c7910c074b91ffde956518d82b3451ea9854f9
JANG EUN JI <omegle3333@gmail.com>
2023-06-08 22:32:04 +0900

    [unrated] Title: 세로 읽기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.js
@@ -0,0 +1,10 @@
+function solution(my_string, m, c) {
+    let answer = '';
+    
+    while(my_string.length) {
+        answer += my_string[c - 1];
+        my_string = my_string.slice(m);
+    }
+    
+    return answer;
+}
